abd6c72973f8b2191aa75610b321af8ca15e4399
PengLei <277201157@qq.com>
2019-05-26 13:55:37 +0800

    更新PC版

--- PC_cn/js/language.js
+++ PC_cn/js/language.js
@@ -31,6 +31,40 @@ $(function(){
         $('.login-form').show()
         $('.register-form').hide()
     })
+
+
+    
+    $('.getCode').click(function(){
+        let num = 60;
+        console.log($(this).text()=='获取验证码')
+        console.log($(this).text())
+        
+        if($(this).text()=='获取验证码'){
+            alert('已向您发送验证码，请注意查收。')
+            this.timer=setInterval(()=>{
+                if(num>0){
+                    $(this).text(num+'S')
+                    num--
+                    
+                }else{
+                    num=60;
+                    clearInterval(this.timer)
+                    $(this).text('获取验证码')
+                }
+               
+            },1000)
+        }
+        
+        
+    })
+
+    $('.register-form li').click(function(){
+        let index = $('.register-form li').index(this);
+        $(this).addClass('active').siblings().removeClass('active')
+        $('.register-form .tabType').eq(index).show().siblings('.tabType').hide()
+        
+
+    })
 });
 
 
--- PC_en/js/language.js
+++ PC_en/js/language.js
@@ -31,6 +31,41 @@ $(function(){
         $('.login-form').show()
         $('.register-form').hide()
     })
+
+
+    
+    
+    $('.getCode').click(function(){
+        let num = 60;
+        console.log($(this).text()=='获取验证码')
+        console.log($(this).text())
+        
+        if($(this).text()=='Send verification code'){
+            alert('已向您发送验证码，请注意查收。')
+            this.timer=setInterval(()=>{
+                if(num>0){
+                    $(this).text(num+'S')
+                    num--
+                    
+                }else{
+                    num=60;
+                    clearInterval(this.timer)
+                    $(this).text('Send verification code')
+                }
+               
+            },1000)
+        }
+        
+        
+    })
+
+    $('.register-form li').click(function(){
+        let index = $('.register-form li').index(this);
+        $(this).addClass('active').siblings().removeClass('active')
+        $('.register-form .tabType').eq(index).show().siblings('.tabType').hide()
+        
+
+    })
 });
 
 
